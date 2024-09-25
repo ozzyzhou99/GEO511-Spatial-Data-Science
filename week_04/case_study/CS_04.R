53c11d5128eda08d5346ed7312f837d1d75c5cfa
ozzyzhou99 <ozzyzhou99@hotmail.com>
2024-09-25 18:17:49 -0400

    Commit 1

--- week_04/case_study/CS_04.R
+++ week_04/case_study/CS_04.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(nycflights13)
+library(maps)
 
 # View the tables
 head(flights)
@@ -13,7 +14,7 @@ farthest_flight <- flights %>%
 # Print the airport code
 farthest_code <- farthest_flight %>% 
   pull(dest)
-print(farthest_code)
+#print(farthest_code)
 
 # Join the table with the one that has the full airport names
 farthest_airport <- farthest_flight %>%
@@ -21,4 +22,25 @@ farthest_airport <- farthest_flight %>%
   select(name) %>%              # Select full name
   as.character()                # Convert the result
 
-print(farthest_airport)
+#print(farthest_airport)
+
+airports %>%
+  distinct(lon,lat) %>%
+  ggplot(aes(lon, lat)) +
+  borders("world") +
+  geom_point(col="red") +
+  coord_quickmap()
+
+# Calculate average delay time
+mean_delay <- flights %>%
+  group_by(dest) %>%
+  summarise(mean_delay = mean(arr_delay, na.rm = TRUE)) %>%
+  left_join(airports, by = c("dest" = "faa"))  # obtain latitude and longitude
+
+# Plot the map
+ggplot(mean_delay, aes(lon, lat)) +
+  borders("state") +  
+  geom_point(aes(color = mean_delay), size = 3) +  
+  scale_color_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0, name = "Mean Delay\n(minutes)") +  
+  coord_fixed(ratio = 1.3, xlim = c(-125, -65), ylim = c(25, 50))
+  
